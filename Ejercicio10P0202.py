3b029fe27890990eda752d19a851cbd0e5b2fb96
jcardonhid <jcardonhid@educacion.navarra.es>
2024-11-11 00:09:06 +0100

    Add files via upload

---
+++ Ejercicio10P0202.py
@@ -0,0 +1,5 @@
+# Lista de la Compra
+
+lista = input("Ingresa una lista de la compra separando los productos por comas:")
+print("La lista de los productos de la compra son:")
+print('', lista.replace(',','\n'))
