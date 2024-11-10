37db226fd6795403cd81278925b9283e1200ddb4
jcardonhid <jcardonhid@educacion.navarra.es>
2024-11-10 23:43:48 +0100

    Add files via upload

---
+++ Ejercicio9P0202.py
@@ -0,0 +1,8 @@
+# Fecha de Nacimiento
+
+fecha = input("Introduce tu fecha de nacimiento en el siguiente formato dd/mm/aaaa:")
+fecha2 = fecha.split('/')
+print('\n')
+print("El día de su nacimiento es:", fecha2[0])
+print("El mes de su nacimiento es:", fecha2[1])
+print("El año de su nacimiento es:", fecha2[2])
